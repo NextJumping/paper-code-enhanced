7bf8b8901dc3c6a26a21024e92ea78a3320cbb75
NextJumping <nextjumping@proton.me>
2021-10-14 05:26:10 +0000

    FTE-11

--- algorithm/DrQ/policy.py
+++ algorithm/DrQ/policy.py
@@ -58,3 +58,96 @@ class Encoder(nn.Module):
             utils.tie_weights(src=source.convs[i], trg=self.convs[i])
 
 
+
+class Actor(nn.Module):
+
+    def __init__(self, obs_shape, action_shape, hidden_dim, hidden_depth,
+                 log_std_bounds, feature_dim=8):
+        super().__init__()
+
+        self.encoder = Encoder(obs_shape, feature_dim)
+
+        self.log_std_bounds = log_std_bounds
+
+        self.trunk = nn.Sequential(
+            nn.Linear(self.encoder.feature_dim + 4, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 2 * action_shape[0])
+        )
+
+
+        self.outputs = dict()
+        self.apply(utils.weight_init)
+
+    def forward(self, obs, others, detach_encoder=False):
+        obs = self.encoder(obs, detach=detach_encoder)
+        obs = torch.cat([obs, others], dim=1)
+        mu, log_std = self.trunk(obs).chunk(2, dim=-1)
+
+        log_std = torch.tanh(log_std)
+        log_std_min, log_std_max = self.log_std_bounds
+        log_std = log_std_min + 0.5 * (log_std_max - log_std_min) * (log_std +
+                                                                     1)
+        std = log_std.exp()
+
+
+        dist = utils.SquashedNormal(mu, std)
+        return dist
+
+
+class Critic(nn.Module):
+
+    def __init__(self, obs_shape, action_shape, hidden_dim, hidden_depth, feature_dim=8):
+        super().__init__()
+
+        self.encoder = Encoder(obs_shape, feature_dim)
+
+        self.Q1 = nn.Sequential(
+            nn.Linear(self.encoder.feature_dim + 5, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 1)
+        )
+
+        self.Q2 = nn.Sequential(
+            nn.Linear(self.encoder.feature_dim + 5, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 1)
+        )
+        self.outputs = dict()
+        self.apply(utils.weight_init)
+
+    def forward(self, obs, action, others, detach_encoder=False):
+        assert obs.size(0) == action.size(0)
+        obs = self.encoder(obs, detach=detach_encoder)
+        obs = torch.cat([obs, others], dim=1)
+
+        obs_action = torch.cat([obs, action], dim=-1)
+        q1 = self.Q1(obs_action)
+        q2 = self.Q2(obs_action)
+
+
+        return q1, q2
+
+
+class Agent(object):
+
+    def __init__(self, obs_shape, action_shape, action_range, device,
+                 discount, init_temperature, lr, actor_update_frequency,
+                 critic_tau, critic_target_update_frequency, batch_size,
+                 log_std_bounds, hidden_dim, hidden_depth, feature_dim):
+        self.action_range = action_range
+        self.device = device
+        self.discount = discount
+        self.critic_tau = critic_tau
+        self.actor_update_frequency = actor_update_frequency
+        self.critic_target_update_frequency = critic_target_update_frequency
+        self.batch_size = batch_size
+
+        self.actor = Actor(obs_shape, action_shape, hidden_dim, hidden_depth,
+                 log_std_bounds, feature_dim).to(device)
