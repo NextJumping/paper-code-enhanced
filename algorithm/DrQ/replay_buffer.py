s5h1420: fix a buffer overflow when checking userspace params

algorithm/DrQ/replay_buffer.py
@@ -7,4 +7,5 @@ import torch.nn.functional as F
 import utils
 
 
-class R
+class ReplayBuffer(object):
+    def __init__(self, obs_shape, action_shape, capa