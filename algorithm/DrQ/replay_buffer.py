8c4bef8a4b2f1d8517a001eaf8ee7c993f21cc8b
NextJumping <nextjumping@proton.me>
2021-11-17 11:24:01 +0000

    working on the admin theme

--- algorithm/DrQ/replay_buffer.py
+++ algorithm/DrQ/replay_buffer.py
@@ -16,4 +16,5 @@ class ReplayBuffer(object):
             nn.ReplicationPad2d(image_pad),
             kornia.augmentation.RandomCrop((obs_shape[-2], obs_shape[-1])))
 
-        self.obses = np.empty((capacity, *obs_shape), dt
+        self.obses = np.empty((capacity, *obs_shape), dtype=np.float32)
+        self.next_obses = np.empty((capacity, *obs_shape), dtype=np.fl
