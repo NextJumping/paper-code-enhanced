e0240fe9ab1756aab9bda5d992b12e4058a7c1bc
NextJumping <nextjumping@proton.me>
2021-11-24 21:40:44 +0000

    Add a note

--- algorithm/DrQ/replay_buffer.py
+++ algorithm/DrQ/replay_buffer.py
@@ -19,4 +19,6 @@ class ReplayBuffer(object):
         self.obses = np.empty((capacity, *obs_shape), dtype=np.float32)
         self.next_obses = np.empty((capacity, *obs_shape), dtype=np.float32)
         self.actions = np.empty((capacity, *action_shape), dtype=np.float32)
-        self.rewards = np.empty((capacity, 1), dtype=np.flo
+        self.rewards = np.empty((capacity, 1), dtype=np.float32)
+        self.not_dones = np.empty((capacity, 1), dtype=np.float32)
+        self.
