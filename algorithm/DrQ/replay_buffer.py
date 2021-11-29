4ca849f3d5728c30b72721fe13d707043dd6652c
NextJumping <nextjumping@proton.me>
2021-11-29 18:30:55 +0000

    tg3: Work around HW/FW limitations with vlan encapsulated frames

--- algorithm/DrQ/replay_buffer.py
+++ algorithm/DrQ/replay_buffer.py
@@ -23,4 +23,11 @@ class ReplayBuffer(object):
         self.not_dones = np.empty((capacity, 1), dtype=np.float32)
         self.not_dones_no_max = np.empty((capacity, 1), dtype=np.float32)
         self.others = np.empty((capacity, 4), dtype=np.float32)
-        self.next_others = np.empty((capa
+        self.next_others = np.empty((capacity, 4), dtype=np.float32)
+
+
+
+        self.idx = 0
+        self.full = False
+
+    def __len__(
