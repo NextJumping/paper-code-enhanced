b3d104f8334c41365818fe0a002f0706565e3255
NextJumping <nextjumping@proton.me>
2022-01-03 09:41:27 +0000

    added basic documentation

--- algorithm/DrQ/utils.py
+++ algorithm/DrQ/utils.py
@@ -123,3 +123,45 @@ class FrameStack(gym.Wrapper):
         assert len(self._frames) == self._k
         return np.concatenate(list(self._frames), axis=0)
 
+
+class TanhTransform(pyd.transforms.Transform):
+    domain = pyd.constraints.real
+    codomain = pyd.constraints.interval(-1.0, 1.0)
+    bijective = True
+    sign = +1
+
+    def __init__(self, cache_size=1):
+        super().__init__(cache_size=cache_size)
+
+    @staticmethod
+    def atanh(x):
+        return 0.5 * (x.log1p() - (-x).log1p())
+
+    def __eq__(self, other):
+        return isinstance(other, TanhTransform)
+
+    def _call(self, x):
+        return x.tanh()
+
+    def _inverse(self, y):
+        return self.atanh(y)
+
+    def log_abs_det_jacobian(self, x, y):
+        return 2. * (math.log(2.) - x - F.softplus(-2. * x))
+
+
+class SquashedNormal(pyd.transformed_distribution.TransformedDistribution):
+    def __init__(self, loc, scale):
+        self.loc = loc
+        self.scale = scale
+
+        self.base_dist = pyd.Normal(loc, scale)
+        transforms = [TanhTransform()]
+        super().__init__(self.base_dist, transforms)
+
+    @property
+    def mean(self):
+        mu = self.loc
+        for tr in self.transforms:
+            mu = tr(mu)
+        return mu
---
+++ algorithm/HTARADrQ/logger.py
@@ -0,0 +1,47 @@
+
+import csv
+import json
+import os
+import shutil
+from collections import defaultdict
+
+import numpy as np
+
+import torch
+import torchvision
+from termcolor import colored
+from torch.utils.tensorboard import SummaryWriter
+
+
+COMMON_TRAIN_FORMAT = [('step', 'S', 'int'),
+                       ('master_loss', 'ML', 'float'), ('sub_loss', 'SL', 'float')
+                       ]
+
+
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self._sum = 0
+        self._count = 0
+
+    def update(self, value, n=1):
+        self._sum += value
+        self._count += n
+
+    def value(self):
+        return self._sum / max(1, self._count)
+
+
+class MetersGroup(object):
+    def __init__(self, file_name, formating):
+        self._csv_file_name = self._prepare_file(file_name, 'csv')
+        self._formating = formating
+        self._meters = defaultdict(AverageMeter)
+        self._csv_file = open(self._csv_file_name, 'w')
+        self._csv_writer = None
+
+    def _prepare_file(self, prefix, suffix):
+        file_name = f'{prefix}.{suffix}'
+        if os.path.exists(file_name):
+            os.remove(file_name)
