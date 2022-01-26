adf43ef97981732135c33820368cfd2b067c609a
NextJumping <nextjumping@proton.me>
2022-01-26 13:48:03 +0000

    Fix rubocop warnings

--- algorithm/HTARADrQ/master_replay_buffer.py
+++ algorithm/HTARADrQ/master_replay_buffer.py
@@ -48,4 +48,5 @@ class ReplayBuffer(object):
 
         obses = torch.as_tensor(obses, device=self.device).float()
         next_obses = torch.as_tensor(next_obses, device=self.device).float()
-        actions = torch.as_tensor(s
+        actions = torch.as_tensor(self.actions[idxs], device=self.device)
+        rewards = torch.as_tensor(s
