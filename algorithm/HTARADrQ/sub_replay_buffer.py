41ba5396a987983a117ec558b0b3e220006b24bc
NextJumping <nextjumping@proton.me>
2022-02-15 16:05:59 +0000

    Update shield

---
+++ algorithm/HTARADrQ/sub_replay_buffer.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+import kornia
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import utils
+
+
+class ReplayBuffer(object):
+    def __init__(self, obs_shape, action_shape, capacity, image_pad, device):
+        self.capacity = capacity
