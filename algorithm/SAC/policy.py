842ba6b0b407d36d293b734db053fb761ff2a8f8
NextJumping <nextjumping@proton.me>
2022-04-14 11:38:02 +0000

    Updated feature list

--- algorithm/SAC/policy.py
+++ algorithm/SAC/policy.py
@@ -96,3 +96,81 @@ class Actor(nn.Module):
         dist = utils.SquashedNormal(mu, std)
         return dist
 
+
+class Critic(nn.Module):
+
+    def __init__(self, obs_shape, action_shape, hidden_dim, hidden_depth, feature_dim=8):
+        super().__init__()
+
+        self.encoder = Encoder(obs_shape, feature_dim)
+
+        self.Q1 = nn.Sequential(
+            nn.Linear(self.encoder.feature_dim + 5, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 1)
+        )
+
+        self.Q2 = nn.Sequential(
+            nn.Linear(self.encoder.feature_dim + 5, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 1)
+        )
+        self.outputs = dict()
+        self.apply(utils.weight_init)
+
+    def forward(self, obs, action, others, detach_encoder=False):
+        assert obs.size(0) == action.size(0)
+        obs = self.encoder(obs, detach=detach_encoder)
+        obs = torch.cat([obs, others], dim=1)
+
+        obs_action = torch.cat([obs, action], dim=-1)
+        q1 = self.Q1(obs_action)
+        q2 = self.Q2(obs_action)
+
+
+        return q1, q2
+
+
+class Agent(object):
+
+    def __init__(self, obs_shape, action_shape, action_range, device,
+                 discount, init_temperature, lr, actor_update_frequency,
+                 critic_tau, critic_target_update_frequency, batch_size,
+                 log_std_bounds, hidden_dim, hidden_depth, feature_dim):
+        self.action_range = action_range
+        self.device = device
+        self.discount = discount
+        self.critic_tau = critic_tau
+        self.actor_update_frequency = actor_update_frequency
+        self.critic_target_update_frequency = critic_target_update_frequency
+        self.batch_size = batch_size
+
+        self.actor = Actor(obs_shape, action_shape, hidden_dim, hidden_depth,
+                 log_std_bounds, feature_dim).to(device)
+
+        self.critic = Critic(obs_shape, action_shape, hidden_dim, hidden_depth, feature_dim).to(device)
+        self.critic_target = Critic(obs_shape, action_shape, hidden_dim, hidden_depth, feature_dim).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+
+        self.actor.encoder.copy_conv_weights_from(self.critic.encoder)
+
+        self.log_alpha = torch.tensor(np.log(init_temperature)).to(device)
+        self.log_alpha.requires_grad = True
+        self.target_entropy = -action_shape[0]
+
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=lr)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),
+                                                 lr=lr)
+        self.log_alpha_optimizer = torch.optim.Adam([self.log_alpha], lr=lr)
+
+        self.train()
+        self.critic_target.train()
+
+
+    def train(self, training=True):
+        self.training = training
+        self.actor.train(training)
