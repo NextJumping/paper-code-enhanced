fb657e0ac5262999e3ec22b6321b4fe11908e41a
NextJumping <nextjumping@proton.me>
2022-04-23 05:48:14 +0000

    Fix #29

--- algorithm/SAC/replay_buffer.py
+++ algorithm/SAC/replay_buffer.py
@@ -8,4 +8,9 @@ import utils
 
 
 class ReplayBuffer(object):
-    def __init__(self, obs_shape, action_shap
+    def __init__(self, obs_shape, action_shape, capacity, device):
+        self.capacity = capacity
+        self.device = device
+
+
+        
