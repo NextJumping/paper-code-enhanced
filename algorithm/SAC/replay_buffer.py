c7525d05c822a5c7574d0eba38a914aeb8409626
NextJumping <nextjumping@proton.me>
2022-05-16 18:16:00 +0000

    Update 4 drones

--- algorithm/SAC/replay_buffer.py
+++ algorithm/SAC/replay_buffer.py
@@ -52,4 +52,5 @@ class ReplayBuffer(object):
         obses = self.obses[idxs]
         next_obses = self.next_obses[idxs]
 
-        obses = torch.as_tensor(ob
+        obses = torch.as_tensor(obses, device=self.device).float()
+        next_obses = torch.as_tensor(next_o
