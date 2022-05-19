e5239ee932737c94ebdafd06615d8cbbe64176ad
NextJumping <nextjumping@proton.me>
2022-05-19 22:19:30 +0000

    composer update mlaa/boss-child

--- algorithm/SAC/replay_buffer.py
+++ algorithm/SAC/replay_buffer.py
@@ -55,4 +55,5 @@ class ReplayBuffer(object):
         obses = torch.as_tensor(obses, device=self.device).float()
         next_obses = torch.as_tensor(next_obses, device=self.device).float()
         actions = torch.as_tensor(self.actions[idxs], device=self.device)
-        rewards = torch.as_tensor(self.rewards[idxs], device
+        rewards = torch.as_tensor(self.rewards[idxs], device=self.device)
+        not_dones_no_max = torch.as_tensor(self.not_dones_no_m
