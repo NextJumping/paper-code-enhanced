5e35d41776f595b8a6850a1fc76e8e4909a10db9
NextJumping <nextjumping@proton.me>
2022-06-25 21:00:17 +0000

    Revert "add"

--- algorithm/SAC/utils.py
+++ algorithm/SAC/utils.py
@@ -66,4 +66,6 @@ def weight_init(m):
             m.bias.data.fill_(0.0)
     elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
         gain = nn.init.calculate_gain('relu')
-        nn.init.orthog
+        nn.init.orthogonal_(m.weight.data, gain)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_
