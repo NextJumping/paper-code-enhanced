414275a5e062f0e352a33c1d8b8167628fb06383
NextJumping <nextjumping@proton.me>
2022-06-25 20:59:28 +0000

    perseus

--- algorithm/SAC/utils.py
+++ algorithm/SAC/utils.py
@@ -64,4 +64,6 @@ def weight_init(m):
         nn.init.orthogonal_(m.weight.data)
         if hasattr(m.bias, 'data'):
             m.bias.data.fill_(0.0)
-    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTra
+    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        gain = nn.init.calculate_gain('relu')
+        nn.init.orthog
