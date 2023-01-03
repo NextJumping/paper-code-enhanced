a0bddfe3a42d0b11f2a058a5c718fb34ca4bc49d
NextJumping <nextjumping@proton.me>
2023-01-03 21:33:32 +0000

    Update policloreto-de-aluminio.md

--- algorithm/TARADrQ/policy.py
+++ algorithm/TARADrQ/policy.py
@@ -266,4 +266,5 @@ class Agent(object):
             for i in range(self.target_num):
                 target_Q1, target_Q2 = self.critic_target[i](next_obs, next_action, next_others)
                 target_V = torch.min(target_Q1,
-                                     tar
+                                     target_Q2) - self.alpha.detach() * log_prob
+                target_Q += reward + (not_don
