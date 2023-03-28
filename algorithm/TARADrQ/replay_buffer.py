623a0cb2a35c9192619e105346df13105dd534a1
NextJumping <nextjumping@proton.me>
2023-03-28 06:35:40 +0000

    Update ru.coffee

--- algorithm/TARADrQ/replay_buffer.py
+++ algorithm/TARADrQ/replay_buffer.py
@@ -44,4 +44,7 @@ class ReplayBuffer(object):
         np.copyto(self.next_others[self.idx], next_others)
 
 
-        self.idx = (self.idx + 1) % 
+        self.idx = (self.idx + 1) % self.capacity
+        self.full = self.full or self.idx == 0
+
+    def sample(self, batch_si
