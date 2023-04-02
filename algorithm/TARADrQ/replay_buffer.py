69ef1d2ee04d4a769301cbbc4c41c206c51026b1
NextJumping <nextjumping@proton.me>
2023-04-02 13:27:08 +0000

    Update python-app.yml

--- algorithm/TARADrQ/replay_buffer.py
+++ algorithm/TARADrQ/replay_buffer.py
@@ -50,4 +50,7 @@ class ReplayBuffer(object):
     def sample(self, batch_size):
         idxs = np.random.randint(0,
                                  self.capacity if self.full else self.idx,
-                               
+                                 size=batch_size)
+
+        obses = self.obses[idxs]
+        next_obses = self.n
