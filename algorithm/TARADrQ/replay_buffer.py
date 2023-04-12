88179c64ac5b2889589922c8e26ca244e3fe75ac
NextJumping <nextjumping@proton.me>
2023-04-12 16:24:27 +0000

    Update cobaltstrike.txt

--- algorithm/TARADrQ/replay_buffer.py
+++ algorithm/TARADrQ/replay_buffer.py
@@ -73,4 +73,7 @@ class ReplayBuffer(object):
         obses = self.aug_trans(obses)
         next_obses = self.aug_trans(next_obses)
 
-        obses_aug =
+        obses_aug = self.aug_trans(obses_aug)
+        next_obses_aug = self.aug_trans(next_obses_aug)
+
+        
