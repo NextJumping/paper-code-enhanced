744e6530211e836a2ac6aa7361eac6e826884daa
NextJumping <nextjumping@proton.me>
2023-04-22 12:31:58 +0000

    fix prospectus

--- algorithm/TARADrQ/utils.py
+++ algorithm/TARADrQ/utils.py
@@ -28,3 +28,7 @@ class eval_mode(object):
         for model, state in zip(self.models, self.prev_states):
             model.train(state)
         return False
+
+
+def soft_update_params(net, target_net, tau):
+    for param, target_param in zip(net
