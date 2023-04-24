cd7167e3c81b23cf8251a5a787a9264451fc592d
NextJumping <nextjumping@proton.me>
2023-04-24 12:40:51 +0000

    update annotations

--- algorithm/TARADrQ/utils.py
+++ algorithm/TARADrQ/utils.py
@@ -32,4 +32,5 @@ class eval_mode(object):
 
 def soft_update_params(net, target_net, tau):
     for param, target_param in zip(net.parameters(), target_net.parameters()):
-        target_param.data.copy_(t
+        target_param.data.copy_(tau * param.data +
+                                (1 - tau) * target_param.data
