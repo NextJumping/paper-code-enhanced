bba6e168a6e809037d8ab8e2342514f2b172cf96
NextJumping <nextjumping@proton.me>
2023-04-27 10:02:35 +0000

    Delete main.go

--- algorithm/TARADrQ/utils.py
+++ algorithm/TARADrQ/utils.py
@@ -48,4 +48,9 @@ def make_dir(*path_parts):
     dir_path = os.path.join(*path_parts)
     try:
         os.mkdir(dir_path)
-    except OSE
+    except OSError:
+        pass
+    return dir_path
+
+
+def tie_weights(src, trg):
